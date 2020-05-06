e6602aa8d28c9448736bb360309435db364052b4
panktishah62 <59693226+panktishah62@users.noreply.github.com>
2020-05-06 11:21:39 +0530

    Add files via upload
    initial commit

---
+++ build/precache-manifest.573fb44633b17a4daef822517f0d7140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e64bc7421843be3d921fc1045d1236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "478b2332d8e32ad3a301",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "59649f531ed3fdf8d680",
+    "url": "/static/js/2.34b619dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34b619dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478b2332d8e32ad3a301",
+    "url": "/static/js/main.9c0e500e.chunk.js"
+  },
+  {
+    "revision": "84e0579a32d12ba8be85",
+    "url": "/static/js/runtime-main.05dcf7c1.js"
+  }
+]);
